a1ca5948f7f06297bc3ab680a355447fc4a89e52
Denis <like-nix@mail.ru>
2022-12-13 21:20:34 +0300

    install ipx
    npm i ipx@latest @netlify/ipx @netlify/plugin-nextjs

--- components/Pages/_Parts/HeaderBlock.tsx
+++ components/Pages/_Parts/HeaderBlock.tsx
@@ -33,7 +33,6 @@ const Container = styled("div")`
     }
 `;
 export const HeaderBlock = ({ data }: TPageGeneric) => {
-    console.log("data", data)
     return (
         <Container>
             {data?.image && (
