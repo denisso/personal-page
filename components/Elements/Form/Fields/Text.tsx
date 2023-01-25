2dc032753277997f6ad825f630a277fcbac9c78c
Denis <Blackbrain2009@gmail.com>
2023-01-25 09:22:29 +0300

    fix styles

--- components/Elements/Form/Fields/Text.tsx
+++ components/Elements/Form/Fields/Text.tsx
@@ -2,66 +2,76 @@ import React from "react";
 import { useField } from "formik";
 import { TFieldText, IValidate } from "../types";
 import TextField from "@mui/material/TextField";
+import styled from "styled-components";
 import * as Yup from "yup";
 
+const FieldContainer = styled("div")`
+  & > .MuiTextField-root {
+    height: 100%;
+    .MuiInputBase-root {
+      height: 100%;
+    }
+  }
+`;
+
 export const FieldText = ({
-    name,
-    label,
-    multiline,
-    disabled,
-    autoFocus,
-    onChange,
+  name,
+  label,
+  multiline,
+  disabled,
+  autoFocus,
+  onChange,
 }: TFieldText) => {
-    const [input, meta] = useField(name);
-    React.useEffect(() => {
-        if (onChange instanceof Function) {
-            onChange(input.value);
-        }
-    }, [input.value, onChange]);
-    return (
-        <div className={`Field ${name}`}>
-            <TextField
-                fullWidth
-                {...(disabled && { disabled: true })}
-                {...(autoFocus && { autoFocus: true })}
-                name={name}
-                label={`${label}${meta.error ? " - " + meta.error : ""}`}
-                value={input.value}
-                {...(multiline && { multiline: true, rows: 4 })}
-                onChange={input.onChange}
-                {...(meta.error && { error: true })}
-            />
-        </div>
-    );
+  const [input, meta] = useField(name);
+  React.useEffect(() => {
+    if (onChange instanceof Function) {
+      onChange(input.value);
+    }
+  }, [input.value, onChange]);
+  return (
+    <FieldContainer className={`Field ${name}`}>
+      <TextField
+        fullWidth
+        {...(disabled && { disabled: true })}
+        {...(autoFocus && { autoFocus: true })}
+        name={name}
+        label={`${label}${meta.error ? " - " + meta.error : ""}`}
+        value={input.value}
+        {...(multiline && { multiline: true, rows: 4 })}
+        onChange={input.onChange}
+        {...(meta.error && { error: true })}
+      />
+    </FieldContainer>
+  );
 };
 
 const rulesDefault: Required<TFieldText["rules"]> = {
-    required: false,
-    min: 1,
-    max: 256,
+  required: false,
+  min: 1,
+  max: 256,
 };
 
 const getSchema = (rulesIn: TFieldText["rules"]): any => {
-    const rulesOut: Required<TFieldText["rules"]> = {
-        ...rulesDefault,
-        ...rulesIn,
-    };
+  const rulesOut: Required<TFieldText["rules"]> = {
+    ...rulesDefault,
+    ...rulesIn,
+  };
 
-    let result: any = Yup.string()
-        .min(rulesOut.min, "Слишком короткое")
-        .max(rulesOut.max, "Слишком длинное");
+  let result: any = Yup.string()
+    .min(rulesOut.min, "Слишком короткое")
+    .max(rulesOut.max, "Слишком длинное");
 
-    if (rulesOut.required) {
-        result = result.required("Обязательное поле");
-    }
+  if (rulesOut.required) {
+    result = result.required("Обязательное поле");
+  }
 
-    return result;
+  return result;
 };
 
 export const validateText: IValidate = (field: TFieldText) => {
-    const { value, ...other } = field;
-    let val = "";
-    if (typeof value === "string") val = value;
-    const result = { schema: getSchema(field["rules"]), ...other, value: val };
-    return result;
+  const { value, ...other } = field;
+  let val = "";
+  if (typeof value === "string") val = value;
+  const result = { schema: getSchema(field["rules"]), ...other, value: val };
+  return result;
 };
--- components/Footer/ModalSearch.tsx
+++ components/Footer/ModalSearch.tsx
@@ -9,170 +9,166 @@ import { TResponseGeneric } from "../../lib/types";
 import Pagination from "@mui/material/Pagination";
 import CloseIcon from "@mui/icons-material/Close";
 import {
-    TSchema,
-    Form,
-    ESubmitStates,
-    BtnSubmit,
-    TStateLabels,
+  TSchema,
+  Form,
+  ESubmitStates,
+  BtnSubmit,
+  TStateLabels,
 } from "../Elements/Form";
 import { FormikHelpers } from "formik";
 import { useSelector } from "react-redux";
 import { selectArticles } from "../../features/state";
 import { styleOneLine } from "../Elements/Form/StylesCases/oneline";
 const ModalStyled = styled(Modal)`
-    transition: width var(--transition), height var(--transition);
-    padding: 1rem;
-    background-color: ${({ theme }) => theme.palette.colorRoot};
-    margin: 0 auto;
+  transition: width var(--transition), height var(--transition);
+  padding: 1rem;
+  background-color: ${({ theme }) => theme.palette.colorRoot};
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    border-radius: 0;
+    width: 100%;
+    height: 100%;
+  }
+  ${({ theme }) => theme.breakpoints.up("sm")} {
+    border-radius: var(--borderRadiusBlock);
+    height: 90%;
+  }
+  ${({ theme }) => theme.breakpoints.between("sm", "lg")} {
+    width: 90%;
+  }
+  ${({ theme }) => theme.breakpoints.between("lg", "xl")} {
+    width: 85%;
+  }
+  ${({ theme }) => theme.breakpoints.up("xl")} {
+    width: 1400px;
+  }
+  .FirstLine {
+    width: 100%;
+    .FormSearch {
+      ${styleOneLine}
+    }
+  }
+
+  .Result {
+    transition: height var(--transition);
+    flex: 1;
+    overflow-y: hidden;
+    .ResultList {
+      overflow-y: auto;
+      height: 100%;
+    }
+  }
+
+  .Pagination {
     display: flex;
-    flex-direction: column;
-
-    ${({ theme }) => theme.breakpoints.down("sm")} {
-        border-radius: 0;
-        width: 100%;
-        height: 100%;
-    }
-    ${({ theme }) => theme.breakpoints.up("sm")} {
-        border-radius: var(--borderRadiusBlock);
-        height: 90%;
-    }
-    ${({ theme }) => theme.breakpoints.between("sm", "lg")} {
-        width: 90%;
-    }
-    ${({ theme }) => theme.breakpoints.between("lg", "xl")} {
-        width: 85%;
-    }
-    ${({ theme }) => theme.breakpoints.up("xl")} {
-        width: 1400px;
-    }
-    .FirstLine {
-        width: 100%;
-        .FormSearch {
-            ${styleOneLine}
-        }
-    }
-
-    .Result {
-        transition: height var(--transition);
-        flex: 1;
-        overflow-y: hidden;
-        .ResultList {
-            overflow-y: auto;
-            height: 100%;
-        }
-    }
-
-    .Pagination {
-        display: flex;
-        justify-content: center;
-        margin-top: 1rem;
-    }
+    justify-content: center;
+    margin-top: 1rem;
+  }
 `;
 
 interface ISearchRequest {
-    (search: string, token?: string): Promise<TResponseGeneric>;
+  (search: string, token?: string): Promise<TResponseGeneric>;
 }
 
 const searchRequest: ISearchRequest = (search, token) => {
-    return new Promise((resolve, reject) => {
-        axios(`/api/search?search=${search}&access-token=${token}`)
-            .then(({ data }: AxiosResponse<TResponseGeneric>) => {
-                resolve(data);
-            })
-            .catch((err: AxiosError) => reject(err.message));
-    });
+  return new Promise((resolve, reject) => {
+    axios(`/api/search?search=${search}&access-token=${token}`)
+      .then(({ data }: AxiosResponse<TResponseGeneric>) => {
+        resolve(data);
+      })
+      .catch((err: AxiosError) => reject(err.message));
+  });
 };
 
 type TFormValues = {
-    search: string;
+  search: string;
 };
 const stateLabels: TStateLabels = {
-    Initial: "Искать",
-    Pending: "",
-    Error: "Неудача, Искать снова",
+  Initial: "Искать",
+  Pending: "",
+  Error: "Неудача, Искать снова",
 };
 
 const ModalSearch = () => {
-    const { isModalOpen, closeModal } = useModal({ modal: "search" });
-    const articles = useSelector(selectArticles);
-    const [result, setResult] = React.useState<Array<TPropsGeneric>>([]);
-    const [stateSubmit, setStateSubmit] = React.useState<ESubmitStates>(
-        ESubmitStates.Initial
-    );
-    const [search, setSearch] = React.useState<string>("");
-    const schema: TSchema = [
-        {
-            text: {
-                name: "search",
-                label: "Строка поиска",
-                autoFocus: true,
-                rules: { required: true, min: 4 },
-                value: search,
-            },
-        },
-    ];
-    const onSubmit = React.useCallback(
-        (values: TFormValues, { setStatus }: FormikHelpers<TFormValues>) => {
-            setStatus({ disabled: true });
-            setStateSubmit(ESubmitStates.Pending);
-            setSearch(values.search);
-            searchRequest(values.search)
-                .then((result: TResponseGeneric) => {
-                    setStateSubmit(ESubmitStates.Successful);
-                    if (Array.isArray(result.data)) {
-                        setResult(result.data);
-                    }
-                    setStatus({ disabled: false });
-                })
-                .catch(() => {
-                    setStateSubmit(ESubmitStates.Error);
-                    setStatus({ disabled: false });
-                });
-        },
-        []
-    );
-    const handleCanel = () => {
-        closeModal();
-        setTimeout(() => setStateSubmit(ESubmitStates.Initial), 300);
-    };
-    return (
-        <ModalStyled open={isModalOpen}>
-            <div className="FirstLine">
-                <Form
-                    schema={schema}
-                    onSubmit={onSubmit}
-                    className="FormSearch"
-                >
-                    <div className="Buttons">
-                        <BtnSubmit
-                            className="ButtonSubmit"
-                            stateSubmit={stateSubmit}
-                            stateLabels={stateLabels}
-                        />
+  const { isModalOpen, closeModal } = useModal({ modal: "search" });
+  const articles = useSelector(selectArticles);
+  const [result, setResult] = React.useState<Array<TPropsGeneric>>([]);
+  const [stateSubmit, setStateSubmit] = React.useState<ESubmitStates>(
+    ESubmitStates.Initial
+  );
+  const [search, setSearch] = React.useState<string>("");
+  const schema: TSchema = [
+    {
+      text: {
+        name: "search",
+        label: "Строка поиска",
+        autoFocus: true,
+        rules: { required: true, min: 4 },
+        value: search,
+      },
+    },
+  ];
+  const onSubmit = React.useCallback(
+    (values: TFormValues, { setStatus }: FormikHelpers<TFormValues>) => {
+      setStatus({ disabled: true });
+      setStateSubmit(ESubmitStates.Pending);
+      setSearch(values.search);
+      searchRequest(values.search)
+        .then((result: TResponseGeneric) => {
+          setStateSubmit(ESubmitStates.Successful);
+          if (Array.isArray(result.data)) {
+            setResult(result.data);
+          }
+          setStatus({ disabled: false });
+        })
+        .catch(() => {
+          setStateSubmit(ESubmitStates.Error);
+          setStatus({ disabled: false });
+        });
+    },
+    []
+  );
+  const handleCanel = () => {
+    closeModal();
+    setTimeout(() => setStateSubmit(ESubmitStates.Initial), 300);
+  };
+  return (
+    <ModalStyled open={isModalOpen}>
+      <div className="FirstLine">
+        <Form schema={schema} onSubmit={onSubmit} className="FormSearch">
+          <div className="Buttons">
+            <BtnSubmit
+              className="ButtonSubmit"
+              stateSubmit={stateSubmit}
+              stateLabels={stateLabels}
+            />
 
-                        <Button onClick={handleCanel} className="ButtonClose">
-                            <CloseIcon className="Icon" />
-                        </Button>
-                    </div>
-                </Form>
-                <div className="Info">
-                    Всего: {articles.articles} найдено: {result.length} Запрос:{" "}
-                    {search}{" "}
-                </div>
-            </div>
+            <Button onClick={handleCanel} className="ButtonClose">
+              <CloseIcon className="Icon" />
+            </Button>
+          </div>
+        </Form>
+        <div className="Info">
+          {"Всего:"} {articles.articles} найдено: {result.length} Запрос:{" "}
+          {search}{" "}
+        </div>
+      </div>
 
-            <div className="Result">
-                <EntitiesLinks
-                    linked={result}
-                    className="ResultList"
-                    callbackClick={closeModal}
-                />
-            </div>
-            <div className="Pagination">
-                <Pagination count={10} disabled />
-            </div>
-        </ModalStyled>
-    );
+      <div className="Result">
+        <EntitiesLinks
+          linked={result}
+          className="ResultList"
+          callbackClick={closeModal}
+        />
+      </div>
+      <div className="Pagination">
+        <Pagination count={10} disabled />
+      </div>
+    </ModalStyled>
+  );
 };
 
 export default ModalSearch;
